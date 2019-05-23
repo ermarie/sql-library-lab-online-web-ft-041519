0f50451a6b8bf1945b6e57cef04eb38bcb6061df
Erin Stalzer <erinstalzer@gmail.com>
2019-05-23 20:36:19 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,17 +11,28 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(*) FROM characters
+  GROUP BY characters.species
+  ORDER BY COUNT(*) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM authors
+  INNER JOIN series ON authors.id = series.author_id
+  INNER JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+  INNER JOIN characters ON series.id = characters.series_id
+  GROUP BY series.title
+  HAVING characters.species = 'human'"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books.series_id) FROM characters
+  INNER JOIN series ON characters.series_id = series.id
+  INNER JOIN books ON series.id = books.series_id
+  GROUP BY characters.name
+  ORDER BY COUNT(books.series_id)"
 end
